285b9e50618835a59d78ade02f6c20dc0ffe458a
Aritra Mukhopadhyay <2001aritramukhopadhyay@gmail.com>
2022-08-25 23:48:37 +0530

    Output short URL fixed and copy to clipboard func added

--- main.js
+++ main.js
@@ -3,22 +3,32 @@
 const btn = document.getElementById("btn");
 btn.addEventListener('click',getData);
 
-
-
-
-
 async function getData(){
     const url = document.getElementById("url").value;
     console.log(url);
     try {
-            const res = await fetch(`https://api.shrtco.de/v2/shorten?url= ${url}`);
-            const data =await res.json();
-            console.log(data.result.short_link);
-            alert(`${url}`+"\n\n"+`${data.result.short_link}`);
+            const res = await fetch(`https://api.shrtco.de/v2/shorten?url=${url}`);
+            const data = await res.json();
+            console.log(data.result.full_short_link);
+            //alert(`${url}`+"\n\n"+`${data.result.short_link}`);
+
+            const newURL = document.getElementById("newURL");
+            newURL.innerHTML= newURL.value= `${data.result.full_short_link}`;
+
+            const copyBtn = document.getElementById("copy");
+            copyBtn.addEventListener('click', ()=>{
+
+                alert("copied");
+                console.log("Copied to clipboard");
+                navigator.clipboard.writeText(`${data.result.full_short_link}`);
+
+            });
         } catch (error) {
                 console.log(error);
             }
 }
+
+
         
         
         
